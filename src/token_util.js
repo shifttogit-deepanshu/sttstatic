6185d91ba02e4ce9f908f33a9c466ba7b8394473
shifttogit-deepanshu <sharmadeepanshu.kaizen1@gmail.com>
2022-05-19 07:41:18 +0530

    url

--- src/token_util.js
+++ src/token_util.js
@@ -7,7 +7,7 @@ export async function getTokenOrRefresh() {
 
     if (speechToken === undefined) {
         try {
-            const res = await axios.get('https://stserver.azurewebsites.net/api/get-speech-token');
+            const res = await axios.get('https://ccserver1.azurewebsites.net/api/get-speech-token');
             const token = res.data.token;
             const region = res.data.region;
             cookie.set('speech-token', region + ':' + token, {maxAge: 540, path: '/'});
